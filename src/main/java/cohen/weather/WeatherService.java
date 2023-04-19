a68a6e286dd422584b392f34edfb28217f70aaf4
MC-Student <meiratova123@gmail.com>
2023-04-19 11:23:30 -0400

    Fixed method declaration style

--- src/main/java/cohen/weather/WeatherService.java
+++ src/main/java/cohen/weather/WeatherService.java
@@ -10,6 +10,7 @@ public interface WeatherService
 {
     @GET("data/2.5/weather?appid=da9ab98f611490083a79456bd71f4370&units=imperial")
     Observable<CurrentWeather> getCurrentWeather(@Query("q") String location);
+
     @GET("data/2.5/forecast?appid=da9ab98f611490083a79456bd71f4370&units=imperial")
     Observable<FiveDayForecast> getFiveDayForecast(@Query("q") String cityName);
 }
